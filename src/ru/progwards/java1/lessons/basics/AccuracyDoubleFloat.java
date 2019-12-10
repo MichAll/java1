666a360535875d241f39ff030d1fc29d5108c7fa
MichAll <mspsiallya@yandex.ru>
2019-12-11 02:32:28 +0300

    Hello start

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -3,15 +3,15 @@ package ru.progwards.java1.lessons.basics;
 public class AccuracyDoubleFloat {
 
     public static double volumeBallDouble(double radius) {
-        double sphereVd = (double)4/3 * 3.14 * Math.pow(radius, 3);
+        double sphereVd = (double)4/3 * 3.14 * radius * radius * radius;
         return sphereVd;
     }
     public static float volumeBallFloat(float radius) {
-        float sphereVf = (float)4/3 * 3.14F * (float)Math.pow(radius, 3);
+        float sphereVf = (float)4/3 * 3.14F * radius *radius * radius;
         return sphereVf;
     }
     public static double calculateAccuracy(double radius) {
-        double radiusAcc = radius - (float)radius;
+        Double radiusAcc = volumeBallDouble(radius) - volumeBallFloat((float)radius);
         return radiusAcc;
     }
     public static void main(String[] args) {
