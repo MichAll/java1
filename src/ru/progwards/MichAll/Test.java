c9c805baddaf48f161aae2cb9787d21fbe92eff0
MichAll <mspsiallya@yandex.ru>
2019-12-25 21:47:21 +0300

    Hello start

--- src/ru/progwards/MichAll/Test.java
+++ src/ru/progwards/MichAll/Test.java
@@ -1,29 +1,37 @@
 package ru.progwards.MichAll;
 
 public class Test {
-    static double fractional(double num) {
-        int int1 = (int)num;
-        double double1 = num - int1;
-        return (double1);
-    }
-    static int addAsStrings(int n1, int n2) {
-        String str1 = Integer.toString(n1);
-        String str2 = Integer.toString(n2);
-        String str3 = str1+str2;
-        int n3 = Integer.parseInt(str3);
-        return n3;
-    }
-    static long factorial(long n) {
-        long startSum = 1;
-        for (long i=1; i<n; i++) {
-            startSum += startSum * i;
-         }
-        return (startSum);
-    }
+//    static double fractional(double num) {
+//        int int1 = (int)num;
+//        double double1 = num - int1;
+//        return (double1);
+//    }
+//    static int addAsStrings(int n1, int n2) {
+//        String str1 = Integer.toString(n1);
+//        String str2 = Integer.toString(n2);
+//        String str3 = str1+str2;
+//        int n3 = Integer.parseInt(str3);
+//        return n3;
+//    }
+//    static long factorial(long n) {
+//        long startSum = 1;
+//        for (long i=1; i<n; i++) {
+//            startSum += startSum * i;
+//         }
+//        return (startSum);
+//    }
+//    public int sumArrayItems(int[] a) {
+//        int sumA = 0;
+//        for (int i=0; i < a.length; i++)
+//            sumA = sumA + a[i];
+//
+//        return sumA;
+//    }
+
     public static void main(String[] args) {
-        double num = 1.53;
-        System.out.println(fractional(num));
-        System.out.println(addAsStrings(2,1));
-        System.out.println(factorial(0));
+//        double num = 1.53;
+//        System.out.println(fractional(num));
+//        System.out.println(addAsStrings(2,1));
+//        System.out.println(factorial(0));
     }
 }
